94b07307165a7ee9cdbcd1f37603098fa95aaa5c
Shikhar Vaish <shikarvaish@gmail.com>
2018-06-24 20:14:36 +0530

    asdsa

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,19 +18,15 @@ import django_heroku
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-)
-STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
+# STATICFILES_DIRS = (
+#         os.path.join(BASE_DIR, 'static'),
+# )
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'mysite/media')
 
-
-
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
