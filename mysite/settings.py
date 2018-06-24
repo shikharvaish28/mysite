f898e18b2aa4a1afcf7e851aa80c7a383f38fb04
Shikhar Vaish <shikarvaish@gmail.com>
2018-06-24 20:42:54 +0530

    asdsa

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname((os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_DIRS = (
