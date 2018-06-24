a1764bcb083a37e3e5e71a2ebc3b95c25b20e63d
Shikhar Vaish <shikarvaish@gmail.com>
2018-06-24 20:28:46 +0530

    asdsa

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_DIRS = (
 #         os.path.join(BASE_DIR, 'static'),
 # )
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'mysite/media')
